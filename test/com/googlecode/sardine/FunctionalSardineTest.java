5472af7ab2263d852cb3e76a968609dac15acc64
dkocher@sudo.ch <dkocher@sudo.ch@bf195e34-ef88-11de-8eb4-853c26953258>
2011-04-04 15:29:18 +0000

    Add more failing test cases.

--- test/com/googlecode/sardine/FunctionalSardineTest.java
+++ test/com/googlecode/sardine/FunctionalSardineTest.java
@@ -16,32 +16,122 @@
 
 package com.googlecode.sardine;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.BasicUserPrincipal;
+import org.apache.http.auth.Credentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringBufferInputStream;
+import java.security.Principal;
 import java.util.List;
+import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 import com.googlecode.sardine.model.Response;
+import com.googlecode.sardine.util.SardineException;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
 
 /**
  * @author David Kocher <mailto:dkocher@sudo.ch>
  */
 public class FunctionalSardineTest {
 
+    protected String TEST_PROPERTIES_FILENAME = "test.properties";
+    protected Properties properties;
+
+    @Before
+    public void setUp() throws Exception {
+        properties = new Properties();
+        properties.load(ClassLoader.getSystemResourceAsStream(TEST_PROPERTIES_FILENAME));
+    }
+
     @Test
     public void testGetSingleFile() throws Exception {
         Sardine sardine = SardineFactory.begin();
-
         final InputStream in = sardine.getInputStream("http://sudo.ch/dav/anon/sardine/single/file");
         assertNotNull(in);
         in.close();
+    }
+
+    @Test
+    public void testBasicPreemptiveAuth() throws Exception {
+        final DefaultHttpClient client = new DefaultHttpClient();
+        final CountDownLatch count = new CountDownLatch(1);
+        client.setCredentialsProvider(new BasicCredentialsProvider() {
+            @Override
+            public Credentials getCredentials(AuthScope authscope) {
+                // Set flag that credentials have been used indicating preemptive authentication
+                count.countDown();
+                return new Credentials() {
+                    public Principal getUserPrincipal() {
+                        return new BasicUserPrincipal("anonymous");
+                    }
+
+                    public String getPassword() {
+                        return "invalid";
+                    }
+                };
+            }
+        });
+        Sardine sardine = SardineFactory.begin();
+        // Send basic authentication header in initial request
+//        sardine.enablePreemptiveAuthentication("http", "sudo.ch", 80, true);
+        try {
+            final List<DavResource> resources = sardine.getResources("http://sudo.ch/dav/basic/");
+            fail("Expected authorization failure");
+        }
+        catch(SardineException e) {
+            // Expect Authorization Failed
+            assertEquals(401, e.getStatusCode());
+            // Make sure credentials have been queried
+            assertEquals("No preemptive authentication attempt", 0, count.getCount());
+        }
+    }
+
+    @Test
+    public void testPutExpectContinue() throws Exception {
+        // Anonymous PUT to restricted resource
+        Sardine sardine = SardineFactory.begin();
+        final String url = "http://sudo.ch/dav/basic/sardine/" + UUID.randomUUID().toString();
+        try {
+            sardine.put(url, new InputStream() {
+                @Override
+                public int read() throws IOException {
+                    fail("Expected authentication to fail without sending any body");
+                    return -1;
+                }
+            });
+            fail("Expected authorization failure");
+        }
+        catch(SardineException e) {
+            // Expect Authorization Required
+            assertEquals(401, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void testBasicAuth() throws Exception {
+        Sardine sardine = SardineFactory.begin(properties.getProperty("username"), properties.getProperty("password"));
+        final List<DavResource> resources = sardine.getResources("http://sudo.ch/dav/basic/");
+        assertNotNull(resources);
+        assertTrue(resources.size() > 0);
+    }
+
+    @Test
+    public void testBasicDigest() throws Exception {
+        Sardine sardine = SardineFactory.begin(properties.getProperty("username"), properties.getProperty("password"));
+        final List<DavResource> resources = sardine.getResources("http://sudo.ch/dav/digest/");
+        assertNotNull(resources);
+        assertTrue(resources.size() > 0);
     }
 
     @Test
