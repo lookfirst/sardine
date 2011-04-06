c628600d40e9d51c36cf99ac507b30cf6aa0b8bd
dkocher@sudo.ch <dkocher@sudo.ch@bf195e34-ef88-11de-8eb4-853c26953258>
2011-04-06 06:26:02 +0000

    Formatting.

--- src/com/googlecode/sardine/impl/HttpCopy.java
+++ src/com/googlecode/sardine/impl/HttpCopy.java
@@ -1,27 +1,42 @@
+/*
+ * Copyright 2009-2011 Jon Stevens et al.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.googlecode.sardine.impl;
 
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-
 import com.googlecode.sardine.util.SardineException;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 
 import java.net.URI;
 
 /**
  * Simple class for making copy a bit easier to deal with. Assumes Overwrite = T.
+ * @version $Id:$
  */
-public class HttpCopy extends HttpEntityEnclosingRequestBase {
-    public HttpCopy(String sourceUrl, String destinationUrl) throws SardineException {
-        this.setHeader("Destination", destinationUrl);
-        this.setHeader("Overwrite", "T");
-        this.setURI(URI.create(sourceUrl));
+public class HttpCopy extends HttpEntityEnclosingRequestBase
+{
+	public HttpCopy(String sourceUrl, String destinationUrl) throws SardineException
+	{
+		this.setHeader("Destination", destinationUrl);
+		this.setHeader("Overwrite", "T");
+		this.setURI(URI.create(sourceUrl));
+	}
 
-        if(sourceUrl.endsWith("/") && !destinationUrl.endsWith("/")) {
-            throw new SardineException("Destinationurl must end with a /", destinationUrl);
-        }
-    }
-
-    @Override
-    public String getMethod() {
-        return "COPY";
-    }
+	@Override
+	public String getMethod()
+	{
+		return "COPY";
+	}
 }
--- src/com/googlecode/sardine/impl/HttpPropFind.java
+++ src/com/googlecode/sardine/impl/HttpPropFind.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2009-2011 Jon Stevens et al.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.googlecode.sardine.impl;
 
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
@@ -6,20 +22,26 @@ import java.net.URI;
 
 /**
  * Simple class for making propfind a bit easier to deal with.
+ *
+ * @version $Id:$
  */
-public class HttpPropFind extends HttpEntityEnclosingRequestBase {
-    public HttpPropFind(String url) {
-        this.setDepth(1);
-        this.setURI(URI.create(url));
-        this.setHeader("Content-Type", "text/xml");
-    }
+public class HttpPropFind extends HttpEntityEnclosingRequestBase
+{
+	public HttpPropFind(String url)
+	{
+		this.setDepth(1);
+		this.setURI(URI.create(url));
+		this.setHeader("Content-Type", "text/xml");
+	}
 
-    @Override
-    public String getMethod() {
-        return "PROPFIND";
-    }
+	@Override
+	public String getMethod()
+	{
+		return "PROPFIND";
+	}
 
-    public void setDepth(int val) {
-        this.setHeader("Depth", String.valueOf(val));
-    }
+	public void setDepth(int val)
+	{
+		this.setHeader("Depth", String.valueOf(val));
+	}
 }
