009c49e15c6f91e696cc8b06af055e9f3c979e08
dkocher@sudo.ch <dkocher@sudo.ch@bf195e34-ef88-11de-8eb4-853c26953258>
2011-04-06 06:35:40 +0000

    Formatting.

--- src/com/googlecode/sardine/impl/GzipDecompressingEntity.java
+++ src/com/googlecode/sardine/impl/GzipDecompressingEntity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2009-2011 Jon Stevens et al.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.googlecode.sardine.impl;
 
 import org.apache.http.HttpEntity;
@@ -9,6 +25,8 @@ import java.util.zip.GZIPInputStream;
 
 /**
  * Simple wrapper for decompressing gzipped {@link InputStream}s on the fly.
+ *
+ * @version $Id$
  */
 public final class GzipDecompressingEntity extends HttpEntityWrapper
 {
