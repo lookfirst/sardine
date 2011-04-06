a55d01240670385cb54d2823319a67cb63926f9f
dkocher@sudo.ch <dkocher@sudo.ch@bf195e34-ef88-11de-8eb4-853c26953258>
2011-04-06 06:41:46 +0000

    Move JAXB factory to utility class.

--- src/com/googlecode/sardine/Factory.java
+++ src/com/googlecode/sardine/Factory.java
@@ -1,14 +1,8 @@
 package com.googlecode.sardine;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
+import com.googlecode.sardine.util.SardineException;
 import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-
-import com.googlecode.sardine.model.ObjectFactory;
-import com.googlecode.sardine.util.SardineException;
 
 /**
  * The factory class is responsible for instantiating the JAXB stuff
@@ -22,49 +16,9 @@ public class Factory
 	protected static Factory instance = new Factory();
 
 	/** */
-	protected static Factory instance() { return instance; }
-
-	/** */
-	private JAXBContext context = null;
-
-	/** */
-	public Factory()
+	protected static Factory instance()
 	{
-		try
-		{
-			if (this.context == null)
-				this.context = JAXBContext.newInstance(ObjectFactory.class);
-		}
-		catch (JAXBException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * @return the JAXBContext
-	 */
-	public JAXBContext getContext()
-	{
-		return this.context;
-	}
-
-	/**
-	 * Note: the unmarshaller is not thread safe, so it must be
-	 * created for every request.
-	 *
-	 * @return the JAXB Unmarshaller
-	 */
-	public Unmarshaller getUnmarshaller() throws SardineException
-	{
-		try
-		{
-			return this.context.createUnmarshaller();
-		}
-		catch (JAXBException e)
-		{
-			throw new SardineException(e);
-		}
+		return instance;
 	}
 
 	/** */
@@ -112,6 +66,6 @@ public class Factory
 	/** */
 	public Sardine begin(String username, String password, SSLSocketFactory sslSocketFactory, HttpRoutePlanner routePlanner, Integer port) throws SardineException
 	{
-		return new SardineImpl(this, username, password, sslSocketFactory, routePlanner, port);
+		return new SardineImpl(username, password, sslSocketFactory, routePlanner, port);
 	}
 }
--- src/com/googlecode/sardine/util/SardineUtil.java
+++ src/com/googlecode/sardine/util/SardineUtil.java
@@ -1,28 +1,18 @@
 package com.googlecode.sardine.util;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.TimeZone;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import com.googlecode.sardine.model.Multistatus;
+import com.googlecode.sardine.model.ObjectFactory;
 import org.apache.http.entity.StringEntity;
 import org.w3c.dom.Element;
 
-import com.googlecode.sardine.model.Multistatus;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Basic utility code. I borrowed some code from the webdavlib for
@@ -32,22 +22,40 @@ import com.googlecode.sardine.model.Multistatus;
  */
 public class SardineUtil
 {
-	/** cached version of getResources() webdav xml GET request */
+
+	/** */
+	public final static JAXBContext CONTEXT;
+
+	static
+	{
+		try
+		{
+			CONTEXT = JAXBContext.newInstance(ObjectFactory.class);
+		}
+		catch (JAXBException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * cached version of getResources() webdav xml GET request
+	 */
 	private static StringEntity GET_RESOURCES = null;
 
 	/**
 	 * Date formats using for Date parsing.
 	 */
 	static final SimpleDateFormat formats[] =
-	{
-		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US),
-		new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US),
-		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'", Locale.US),
-		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US),
-		new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US),
-		new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US),
-		new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US)
-	};
+			{
+					new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US),
+					new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US),
+					new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'", Locale.US),
+					new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US),
+					new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US),
+					new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US),
+					new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US)
+			};
 
 	/**
 	 * GMT timezone.
@@ -63,40 +71,16 @@ public class SardineUtil
 	}
 
 	/**
-	 * Hides the irritating declared exception.
-	 */
-	public static String encode(String value)
-	{
-		try
-		{
-			return URLEncoder.encode(value, "utf-8");
-		}
-		catch (UnsupportedEncodingException ex) { throw new RuntimeException(ex); }
-	}
-
-	/**
-	 * Hides the irritating declared exception.
-	 * @return null if there is an IllegalArgumentException
-	 * @throws RuntimeException if there is an UnsupportedEncodingException
-	 */
-	public static String decode(String value)
-	{
-		try
-		{
-			return URLDecoder.decode(value, "utf-8");
-		}
-		catch (UnsupportedEncodingException ex) { throw new RuntimeException(ex); }
-		catch (IllegalArgumentException ex) { return null; }
-	}
-
-	/**
 	 * Loops over all the possible date formats and tries to find the right one.
+	 *
 	 * @param dateValue
 	 */
 	public static Date parseDate(String dateValue)
 	{
 		if (dateValue == null)
+		{
 			return null;
+		}
 
 		Date date = null;
 		for (int i = 0; (date == null) && (i < formats.length); i++)
@@ -116,14 +100,6 @@ public class SardineUtil
 		return date;
 	}
 
-    /**
-	 * Is the status code 2xx
-	 */
-	public static boolean isGoodResponse(int statusCode)
-	{
-		return ((statusCode >= 200) && (statusCode <= 299));
-	}
-
 	/**
 	 * Stupid wrapper cause it needs to be in a try/catch
 	 */
@@ -134,9 +110,9 @@ public class SardineUtil
 			try
 			{
 				GET_RESOURCES = new StringEntity("<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n" +
-													"<propfind xmlns=\"DAV:\">\n" +
-													"	<allprop/>\n" +
-													"</propfind>", "UTF-8");
+						"<propfind xmlns=\"DAV:\">\n" +
+						"	<allprop/>\n" +
+						"</propfind>", "UTF-8");
 			}
 			catch (UnsupportedEncodingException e)
 			{
@@ -150,7 +126,7 @@ public class SardineUtil
 	/**
 	 * Build PROPPATCH entity.
 	 */
-	public static StringEntity getResourcePatchEntity(Map<String,String> setProps, List<String> removeProps)
+	public static StringEntity getResourcePatchEntity(Map<String, String> setProps, List<String> removeProps)
 	{
 		StringEntity patchEntity = null;
 
@@ -159,11 +135,11 @@ public class SardineUtil
 			StringBuffer buf = new StringBuffer("<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n");
 			buf.append("<D:propertyupdate xmlns:D=\"DAV:\" xmlns:S=\"SAR:\">\n");
 
-			if(setProps != null)
+			if (setProps != null)
 			{
 				buf.append("<D:set>\n");
 				buf.append("<D:prop>\n");
-				for(Map.Entry<String,String> prop : setProps.entrySet())
+				for (Map.Entry<String, String> prop : setProps.entrySet())
 				{
 					buf.append("<S:");
 					buf.append(prop.getKey()).append(">");
@@ -174,11 +150,11 @@ public class SardineUtil
 				buf.append("</D:set>\n");
 			}
 
-			if(removeProps != null)
+			if (removeProps != null)
 			{
 				buf.append("<D:remove>\n");
 				buf.append("<D:prop>\n");
-				for(String removeProp : removeProps)
+				for (String removeProp : removeProps)
 				{
 					buf.append("<S:");
 					buf.append(removeProp).append("/>");
@@ -203,27 +179,42 @@ public class SardineUtil
 	/**
 	 * Helper method for getting the Multistatus response processor.
 	 */
-	public static Multistatus getMulitstatus(Unmarshaller unmarshaller, HttpResponse response, String url)
-		throws SardineException
+	public static Multistatus getMultistatus(InputStream stream)
+			throws SardineException
 	{
 		try
 		{
-			return (Multistatus) unmarshaller.unmarshal(response.getEntity().getContent());
+			return (Multistatus) createUnmarshaller().unmarshal(stream);
 		}
 		catch (JAXBException ex)
 		{
-			throw new SardineException("Problem unmarshalling the data", url, ex);
+			throw new SardineException("Problem unmarshalling the data", ex);
 		}
-		catch (IOException ex)
+	}
+
+	/**
+	 * Creates an {@link Unmarshaller} from the {@link SardineUtil#CONTEXT}.
+	 * Note: the unmarshaller is not thread safe, so it must be created for every request.
+	 *
+	 * @return a new Unmarshaller.
+	 * @throws JAXBException
+	 */
+	public static Unmarshaller createUnmarshaller()
+	{
+		try
 		{
-			throw new SardineException(ex);
+			return CONTEXT.createUnmarshaller();
+		}
+		catch (JAXBException e)
+		{
+			throw new RuntimeException("Could not create unmarshaller", e);
 		}
 	}
 
 	/** */
-	public static Map<String,String> extractCustomProps(List<Element> elements)
+	public static Map<String, String> extractCustomProps(List<Element> elements)
 	{
-		Map<String,String> customPropsMap = new HashMap<String,String>(elements.size());
+		Map<String, String> customPropsMap = new HashMap<String, String>(elements.size());
 
 		for (Element element : elements)
 		{
