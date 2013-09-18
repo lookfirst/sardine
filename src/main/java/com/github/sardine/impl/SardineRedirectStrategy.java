bbf74ad0f52e40e53c20288993f14a7045692aba
David Kocher <dkocher@sudo.ch>
2013-09-18 16:42:44 +0200

    Simplify implementation (using new API) allowing redirects for PROPFIND and defaults of super only.

--- src/main/java/com/github/sardine/impl/SardineRedirectStrategy.java
+++ src/main/java/com/github/sardine/impl/SardineRedirectStrategy.java
@@ -16,66 +16,45 @@
 
 package com.github.sardine.impl;
 
-import org.apache.http.Header;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.ProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.protocol.HttpContext;
 
-import com.github.sardine.impl.methods.HttpAcl;
-import com.github.sardine.impl.methods.HttpLock;
 import com.github.sardine.impl.methods.HttpPropFind;
 
 /**
-* @version $Id:$
-*/
+ * @version $Id:$
+ */
 public class SardineRedirectStrategy extends DefaultRedirectStrategy {
+
     @Override
-    public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context) throws ProtocolException
-    {
-        int statusCode = response.getStatusLine().getStatusCode();
-        String method = request.getRequestLine().getMethod();
-        Header locationHeader = response.getFirstHeader("location");
-        switch (statusCode)
-        {
-            case HttpStatus.SC_MOVED_TEMPORARILY:
-                return (method.equalsIgnoreCase(HttpGet.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpHead.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpLock.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpAcl.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpPropFind.METHOD_NAME)) && (locationHeader != null);
-            case HttpStatus.SC_MOVED_PERMANENTLY:
-            case HttpStatus.SC_TEMPORARY_REDIRECT:
-                return method.equalsIgnoreCase(HttpGet.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpHead.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpLock.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpAcl.METHOD_NAME)
-                        || method.equalsIgnoreCase(HttpPropFind.METHOD_NAME);
-            case HttpStatus.SC_SEE_OTHER:
-                return true;
-            default:
-                return false;
+    protected boolean isRedirectable(String method) {
+        if(super.isRedirectable(method)) {
+            return true;
         }
+        return method.equalsIgnoreCase(HttpPropFind.METHOD_NAME);
     }
 
     @Override
     public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context)
-            throws ProtocolException
-    {
+            throws ProtocolException {
         String method = request.getRequestLine().getMethod();
-        if (method.equalsIgnoreCase(HttpPropFind.METHOD_NAME))
-        {
-            return new HttpPropFind(this.getLocationURI(request, response, context));
-        }
-        if (method.equalsIgnoreCase(HttpLock.METHOD_NAME))
-        {
-            return new HttpLock(this.getLocationURI(request, response, context));
+        if(method.equalsIgnoreCase(HttpPropFind.METHOD_NAME)) {
+            return this.copyEntity(new HttpPropFind(this.getLocationURI(request, response, context)), request);
         }
         return super.getRedirect(request, response, context);
+    }
+
+    private HttpUriRequest copyEntity(
+            final HttpEntityEnclosingRequestBase redirect, final HttpRequest original) {
+        if(original instanceof HttpEntityEnclosingRequest) {
+            redirect.setEntity(((HttpEntityEnclosingRequest) original).getEntity());
+        }
+        return redirect;
     }
 }
