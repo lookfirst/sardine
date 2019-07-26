d843bd7d2a3c34bdc8418e72506e882d8d93ba91
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2019-07-25 21:47:23 -0500

    formatting fix

--- src/main/java/com/github/sardine/SardineFactory.java
+++ src/main/java/com/github/sardine/SardineFactory.java
@@ -45,20 +45,19 @@ public final class SardineFactory
 	}
 
 	/**
-	 * @param username Use in authentication header credentials
-	 * @param password Use in authentication header credentials
-	 * @param proxy	Proxy configuration
-	 * @param allowAllCertificates Allow all ssl
-	 * @param threadCount The number of threads you will be fetching with. Used for setting proper max routes.
-	 * @param useIpAddressForSslConnections  Use the ip address of the server when doing SSL verification.
+	 * @param username                      Use in authentication header credentials
+	 * @param password                      Use in authentication header credentials
+	 * @param proxy                         Proxy configuration
+	 * @param allowAllCertificates          Allow all ssl
+	 * @param threadCount                   The number of threads you will be fetching with. Used for setting proper max routes.
+	 * @param useIpAddressForSslConnections Use the ip address of the server when doing SSL verification.
 	 */
 	public static Sardine begin(String username,
 															String password,
 															int threadCount,
 															boolean allowAllCertificates,
 															boolean useIpAddressForSslConnections,
-															ProxySelector proxy)
-	{
+															ProxySelector proxy) {
 		return new SardineImpl(username, password, threadCount, allowAllCertificates, useIpAddressForSslConnections, proxy);
 	}
 }
